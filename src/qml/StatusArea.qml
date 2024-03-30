mathew-dennis/cutie-panel

commit f56b8eab845fb38c0558ec01e91eaa7ab601f870

treewide: adjust positioning and transitions
- fix closing transition
- make quicksetting closing mousearea larger
- make quicksettings one pixel taller than the screen to counter rounding errors when scaling
- fix brighness icon positioning

src/qml/StatusArea.qml
@@ -155,6 +155,7 @@ Item {
 
             onPressed: {
                 settingsState.state = "opening";
+                settingsState.height = Screen.height + 1;
                 settingSheet.setSettingContainerY(-Screen.height);
                 settingSheet.containerOpacity = 0;
                 settingSheet.y = 0;

src/qml/Lockscreen.qml
@@ -7,7 +7,7 @@ Item {
 	id: lockscreen
     visible: true
     width: Screen.width
-    height: Screen.height
+    height: Screen.height + 1
 
 	function timeChanged() {
         lockscreenTime.text = Qt.formatDateTime(new Date(), "HH:mm");
@@ -21,6 +21,7 @@ Item {
 		to: 0
 
 		onFinished: {
+            settingsState.height = setting.height;
 			lockscreen.visible = false;
 		}
 	}

src/qml/SettingSheet.qml
@@ -6,8 +6,8 @@ import Cutie
 Item {
     id: settingSheet
     width: Screen.width
-    height: Screen.height
-    y: -Screen.height
+    height: Screen.height + 1
+    y: -(Screen.height + 1)
 
     property alias containerOpacity: settingContainer.opacity
     property string wifiIcon: "icons/network-wireless-offline.svg"
@@ -235,20 +235,23 @@ Item {
     Item {
         id: dragArea
         x: 0
-        y: parent.height - 10
-        height: 10
+        y: parent.height - height
+        height: 30
         width: parent.width
 
         MouseArea {
-            drag.target: parent; drag.axis: Drag.YAxis; drag.minimumY: - 10; drag.maximumY: Screen.height - 10
+            drag.target: parent
+            drag.axis: Drag.YAxis
+            drag.minimumY: -parent.height
+            drag.maximumY: Screen.height - parent.height
             enabled: settingsState.state != "closed"
             anchors.fill: parent
             propagateComposedEvents: true
 
             onPressed: {
                 settingsState.state = "closing";
-                settingContainer.opacity = parent.y + 10 / Screen.height;
-                settingContainer.y = parent.y + 10 - Screen.height;
+                settingContainer.opacity = (parent.y + parent.height) / Screen.height;
+                settingContainer.y = parent.y - Screen.height;
             }
 
             onReleased: {
@@ -263,8 +266,8 @@ Item {
 
             onPositionChanged: {
                 if (drag.active) {
-                    settingContainer.opacity = parent.y + 10 / Screen.height;
-                    settingContainer.y = parent.y + 10 - Screen.height;
+                    settingContainer.opacity = (parent.y + parent.height) / Screen.height;
+                    settingContainer.y = parent.y - Screen.height;
                 }
             }
         }
@@ -277,27 +280,30 @@ Item {
         width: parent.width
         z: 4
 
+        onOpacityChanged: {
+            if (opacity === 0
+                && settingsState.state === "closed"
+                && !lockscreen.visible) {
+                settingsState.height = setting.height;
+            }
+        }
+
         state: settingsState.state
 
         states: [
             State {
                 name: "opened"
                 PropertyChanges { target: settingContainer; y: 0; opacity: 1 }
-                PropertyChanges { target: dragArea; y: Screen.height - 10}
-                PropertyChanges { target: settingsState; height: Screen.height }
+                PropertyChanges { target: dragArea; y: Screen.height - dragArea.height }
             },
             State {
                 name: "closed"
-                PropertyChanges { target: settingContainer; y: -Screen.height; opacity: 0 }
             },
             State {
                 name: "opening"
-                PropertyChanges { target: settingContainer; y: -Screen.height }
             },
             State {
                 name: "closing"
-                PropertyChanges { target: settingContainer; y: 0 }
-                PropertyChanges { target: settingsState; height: Screen.height }
             }
         ]
 
@@ -312,8 +318,8 @@ Item {
             Transition {
                 to: "closed"
                 ParallelAnimation {
-                    NumberAnimation { target: settingContainer; properties: "y"; duration: 250; easing.type: Easing.InOutQuad; }
-                    NumberAnimation { target: settingContainer; properties: "opacity"; duration: 250; easing.type: Easing.InOutQuad; }
+                    NumberAnimation { target: settingContainer; properties: "y"; duration: 250; easing.type: Easing.InOutQuad; to: -(Screen.height + 1) }
+                    NumberAnimation { target: settingContainer; properties: "opacity"; duration: 250; easing.type: Easing.InOutQuad; to: 0}
                 }
             }
         ]
@@ -490,7 +496,7 @@ Item {
             anchors.left: parent.left
             anchors.bottom: parent.bottom
             anchors.leftMargin: 10
-            anchors.bottomMargin: 50
+            anchors.bottomMargin: 60
             source: "icons/gpm-brightness-lcd-disabled.svg"
             sourceSize.height: height*2
             sourceSize.width: width*2
@@ -510,7 +516,7 @@ Item {
             anchors.right: parent.right
             anchors.bottom: parent.bottom
             anchors.rightMargin: 10
-            anchors.bottomMargin: 50
+            anchors.bottomMargin: 60
             source: "icons/gpm-brightness-lcd"
             sourceSize.height: height*2
             sourceSize.width: width*2

src/qml/main.qml
@@ -5,7 +5,7 @@ Item {
 	id: settingsState
 
 	width: Screen.width
-	height: (lockscreen.visible || state != "closed") ? Screen.height : 30
+	height: Screen.height + 1
 
 	state: "closed" 
 	states: [
@@ -89,6 +89,7 @@ Item {
 				lockscreen.visible = true;
 				lockscreen.opacity = 1;
 				settingsState.state = "closed";
+				settingsState.height = Screen.height + 1;
 			}
 		}
 	}

commit 229c999539e882ced1086323fd96b838d18108cd

StatusArew: fix canceling opening quicksetting panel
Commit f56b8eab845fb38c0558ec01e91eaa7ab601f870 introduced a regression where the panel window would resize to full screen even when openen quicksettings was cancelled blocking input to any other surfaces.

src/qml/StatusArea.qml
@@ -139,13 +139,14 @@ Item {
             anchors.fill: parent
 
             onReleased: {
-                if (parent.y > parent.height) { 
-                    settingsState.state = "opened"
-                }
+                if (parent.y > parent.height)
+                    settingsState.state = "opened";
                 else { 
-                    settingsState.state = "closed"
+                    settingsState.state = "closed";
+                    if (!lockscreen.visible)
+                        settingsState.height = setting.height;
                 }
-                parent.y = 0
+                parent.y = 0;
             }
             
             onPositionChanged: {