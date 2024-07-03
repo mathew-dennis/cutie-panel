4c16cb66cfca740ac14bdf3c7464a7ec29b78d27
Erik Inkinen <erik.inkinen@erikinkinen.fi>
2024-07-03 14:54:44 +0300

    main.cpp: Remove unnecessary qt version check
    We do not support building against Qt versions lower than Qt 6.0.0. Therefore, it is not necessary to check for this.

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,6 @@ int main(int argc, char *argv[])
 {
 	LayerShellQt::Shell::useLayerShell();
 
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-	QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
-
 	QCoreApplication::setOrganizationName("Cutie Community Project");
 	QCoreApplication::setApplicationName("Cutie Panel");
 
