b0d472cf28e5f1929beb9374c36fbd352564add1
mathew-dennis <78794223+mathew-dennis@users.noreply.github.com>
2026-05-28 22:31:27 +0530

    workaround for handiling virtual panel in snapdragon devices

--- src/quicksettings.cpp
+++ src/quicksettings.cpp
@@ -20,13 +20,16 @@ QuickSettings::QuickSettings(QObject *parent)
 	refreshBatteryInfo();
 
 	udevInstance = udev_new();
-	udevEnumerator = udev_enumerate_new(udevInstance);
-	udev_enumerate_add_match_subsystem(udevEnumerator, "backlight");
-	udev_enumerate_scan_devices(udevEnumerator);
-	udevEntry = udev_enumerate_get_list_entry(udevEnumerator);
-	const char *udevPath = udev_list_entry_get_name(udevEntry);
-	udevDevice = udev_device_new_from_syspath(udevInstance, udevPath);
-	if (!udevDevice)
+    udevDevice = udev_device_new_from_syspath(udevInstance, "/sys/class/backlight/panel0-backlight");
+    if (!udevDevice) {
+        udevEnumerator = udev_enumerate_new(udevInstance);
+        udev_enumerate_add_match_subsystem(udevEnumerator, "backlight");
+        udev_enumerate_scan_devices(udevEnumerator);
+        udevEntry = udev_enumerate_get_list_entry(udevEnumerator);
+        const char *udevPath = udev_list_entry_get_name(udevEntry);
+        udevDevice = udev_device_new_from_syspath(udevInstance, udevPath);
+	}
+    if (!udevDevice) 
 		udevDevice = udev_device_new_from_syspath(
 			udevInstance, "/sys/class/leds/lcd-backlight");
 	p_maxBrightness = QString(udev_device_get_sysattr_value(
